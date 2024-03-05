409312de61189c53ca08efaa486e6cbf606ca79d
Nelisiwe1 <nelisiwengqeme3@gmail.com>
2024-03-06 01:55:07 +0200

    about

--- virtual-design/src/components/About.jsx
+++ virtual-design/src/components/About.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import '../index.css'; // Import CSS file for styling
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-container">
+      <h1>About TSN</h1>
 
-export default About
+      <img src="living-room.jpg" alt="About pic" />
+
+      <div className="about-content">
+        <h2>About TSN Interior Design</h2>
+        <p>
+          Welcome to TSN Interior Design, where creativity meets functionality
+          to transform spaces into captivating environments. At TSN, we believe
+          that every space has its own unique story waiting to be told, and our
+          mission is to bring that story to life through innovative design
+          solutions.
+        </p>
+
+        {/* <h2>Our Vision</h2>
+        <p>
+          At TSN Interior Design, our vision is to redefine interior spaces by
+          blending aesthetics with practicality, creating environments that
+          inspire and enrich the lives of those who inhabit them. We strive to
+          set new standards in the industry by seamlessly integrating timeless
+          design principles with modern innovations.
+        </p>
+
+        <h2>Our Approach</h2>
+        <p>
+          TSN Interior Design takes a collaborative approach to every project,
+          working closely with our clients to understand their vision,
+          preferences, and functional requirements. Our team of experienced
+          designers utilizes their expertise to conceptualize and execute
+          designs that exceed expectations while staying true to our client's
+          vision.
+        </p>
+
+        <p>
+          We believe in the power of attention to detail and craftsmanship,
+          ensuring that every aspect of our designs reflects quality and
+          sophistication. From concept development to final execution, we are
+          committed to delivering exceptional results that surpass our clients'
+          aspirations.
+        </p>
+
+        <h2>Our Commitment</h2>
+        <p>
+          At TSN Interior Design, we are committed to excellence in everything
+          we do. From concept to completion, we prioritize client satisfaction,
+          innovation, and integrity. Our dedication to quality craftsmanship and
+          attention to detail sets us apart, making us the trusted choice for
+          discerning clients seeking exceptional interior design solutions.
+        </p> */}
+
+        <button>Contact us</button>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- virtual-design/src/components/Services.jsx
+++ virtual-design/src/components/Services.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <h1>Our Services</h1>
+      
+
+At TSN Interior Design, we offer a comprehensive range of services tailored to meet the diverse needs of our clients. Whether it's residential, commercial, or hospitality design, our team is equipped to handle projects of any scale and complexity.
+
+Our services include:
+
+Residential Design: Transforming houses into homes, we specialize in creating personalized living spaces that reflect our clients' lifestyles and tastes.
+Commercial Design: From offices to retail spaces, we design environments that enhance productivity, brand identity, and customer experience.
+Hospitality Design: Crafting memorable experiences, we design hotels, restaurants, and resorts that captivate guests and leave a lasting impression.
+Consultation and Project Management: We provide expert consultation and project management services to ensure seamless execution and timely completion of every project.
+    </div>
   )
 }
 
